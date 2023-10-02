c79e9c24a1080951f282a4ec59eac7b02f1f5b26
lauraexar <laura.exar@duke.edu>
2023-10-02 12:39:45 -0400

    Read data from data file and process

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,38 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Laura Exar (laura.exar@duke.edu)
+# Date:   Fall 2023
+#--------------------------------------------------------------
+
+#Create a variable pointing to the data file
+file_name = './data/raw/Sara.txt'
+
+#Create a file object from the file
+file_object = open(file_name,'r')
+
+#Read contents of file into a list
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
+
+#Pretend we read one line of data from the file
+lineString = line_list[200]
+
+#Split the string into a list of data items
+lineData = lineString.split()
+
+#Extract items in list into variables
+record_id = lineData[0]
+obs_date = lineData[2]
+obs_lc = lineData[4]
+obs_lat = lineData[6]
+obs_lon = lineData[7]
+
+#Print the location of sara
+print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
